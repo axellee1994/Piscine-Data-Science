25921ff91d3cf98a4b44e7977640924c01593c99
axellee1994 <axellee1994@gmail.com>
2025-09-13 13:51:10 +0800

    Completed ex00

--- Data_Engineer/ex00/setup.sh
+++ Data_Engineer/ex00/setup.sh
@@ -13,12 +13,12 @@ DB_PASSWORD="mysecretpassword"
 echo "Creating database"
 
 # Drop the database if it already exists (optional)
-sudo -u postgres psql -c "DROP DATABASE IF EXISTS $DB_NAME;"
-sudo -u postgres psql -c "DROP USER IF EXISTS $DB_USER;"
+(sudo -u postgres psql -c "DROP DATABASE IF EXISTS $DB_NAME;") 2>/dev/null
+(sudo -u postgres psql -c "DROP USER IF EXISTS $DB_USER;") 2>/dev/null
 
-sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_OWNER;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+(sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';") 2>/dev/null
+(sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_OWNER;") 2>/dev/null
+(sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;") 2>/dev/null
 
 echo "Database created"
 
